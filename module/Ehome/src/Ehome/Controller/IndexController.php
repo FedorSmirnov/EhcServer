b5a7903334803bc2d953e002157676346a0a74f7
Jochen1980 <info@jochen-bauer.net>
2013-11-14 16:03:34 +0100

    ehome module adaptions;

---
+++ module/Ehome/src/Ehome/Controller/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Ehome\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class IndexController extends AbstractActionController{
+	
+	
+	public function indexAction(){
+		
+	}
+	
+	
+	
+}
+
+
+?>
