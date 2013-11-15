f76a86cdbf15e8099f18a7273b7729f60ca192a7
Jochen1980 <info@jochen-bauer.net>
2013-11-15 13:52:01 +0100

    added db connection

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,5 +12,21 @@
  */
 
 return array(
-    // ...
+    'service_manager' => array(
+    	'factories' => array(
+    		'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory'
+    	),
+    	'aliases' => array(
+    		'db' => 'Zend\Db\Adapter\Adapter',
+    	),
+    ),
+	'db' => array(
+		'driver'    => 'pdo',
+		'dsn'       => 'mysql:dbname=xxx;host=localhost',
+		'username'  => 'xxx',
+		'password'  => 'xxx',
+		'driver_options' => array(
+			PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+		),
+	),
 );
