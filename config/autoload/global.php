e65a6193ebca59317731b1bdc0abb55216fdfcb2
fedor <fedor@FedorsLaptop>
2014-01-03 14:11:30 +0100

    moved password and username information into the local.php file

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -10,23 +10,21 @@
  * control, so do not include passwords or other sensitive information in this
  * file.
  */
-
 return array(
-    'service_manager' => array(
-    	'factories' => array(
-    		'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory'
-    	),
-    	'aliases' => array(
-    		'db' => 'Zend\Db\Adapter\Adapter',
-    	),
-    ),
-	'db' => array(
-		'driver'    => 'pdo',
-		'dsn'       => 'mysql:dbname=xxx;host=localhost',
-		'username'  => 'xxx',
-		'password'  => 'xxx',
-		'driver_options' => array(
-			PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
-		),
-	),
+        'service_manager' => array(
+                'factories' => array(
+                        'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory'
+                ),
+                'aliases' => array(
+                        'db' => 'Zend\Db\Adapter\Adapter'
+                )
+        ),
+        'db' => array(
+                'driver' => 'pdo',
+                'dsn' => 'mysql:dbname=local_test;host=localhost',
+                
+                'driver_options' => array(
+                        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+                )
+        )
 );
